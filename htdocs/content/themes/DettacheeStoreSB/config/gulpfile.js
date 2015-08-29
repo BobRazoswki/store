0a8c43c366d3cfc560d7a9746a0e64a8d586c274
BobRazoswki <Bob@MacBook-Pro.netgear.com>
2015-08-29 13:40:23 +0200

    chartreuse work

--- htdocs/content/themes/DettacheeStoreSB/config/gulpfile.js
+++ htdocs/content/themes/DettacheeStoreSB/config/gulpfile.js
@@ -1,76 +1,141 @@
-// npm install gulp gulp-jsmin gulp-clean gulp-sass del gulp-livereload gulp-cache gulp-concat gulp-rename gulp-imagemin gulp-ruby-sass gulp-autoprefixer gulp-minify-css gulp-uglify --save-dev
-'use strict';
-var gulp         = require('gulp'),
-    sass         = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss    = require('gulp-minify-css'),
-    uglify       = require('gulp-uglify'),
-    imagemin     = require('gulp-imagemin'),
-    rename       = require('gulp-rename'),
-    concat       = require('gulp-concat'),
-    cache        = require('gulp-cache'),
-    livereload   = require('gulp-livereload'),
-    del          = require('del'),
-    sass         = require('gulp-sass'),
-    clean        = require('gulp-clean'),
-    jsmin        = require("gulp-jsmin");
+// npm install gulp-rename gulp-sass gulp-bower gulp-autoprefixer gulp-concat gulp-watch gulp-connect gulp-livereload gulp-jsmin gulp-cssmin browser-sync --save-dev
 
-gulp.task('default', function() {
-    gulp.start('styles', 'scripts','depcss','depjs', 'images', 'watch');
+
+
+var livereload = require('gulp-livereload');
+
+var gulp       = require("gulp"),
+    sass       = require("gulp-sass"),
+    autoprefix = require("gulp-autoprefixer"),
+    concat     = require("gulp-concat"),
+
+    watch      = require("gulp-watch"),
+    connect    = require("gulp-connect"),
+    browserSync = require('browser-sync'),
+
+    jsmin      = require("gulp-jsmin"),
+    cssmin     = require("gulp-cssmin"),
+    rename     = require("gulp-rename"),
+
+    bower      = require('gulp-bower'),
+    install    = require("gulp-install"),
+
+    devpath   = "../"
+    buildpath = "../build/"
+    paths = {
+      dev: {
+          js:        devpath + "assets/js/"
+        , css:       devpath + "assets/css/"
+        , img:       devpath + "assets/img/*"
+        , jsfiles:   devpath + "assets/js/partials/*.js"
+        , js2build:   devpath + "assets/js/main.js"
+        , js2files:   devpath + "assets/js/lib/**/*.js"
+        , cssfiles:  devpath + "assets/css/*.css"
+        , scssfiles: devpath + "assets/scss/**/*.scss"
+        , phpfiles: devpath + "./**/*.php"
+        , htmlfiles: devpath + "./*.html"
+        , libfiles: devpath + "assets/lib/**/*"
+      }
+      , build: {
+          js:  buildpath + "assets/js/"
+        , css: buildpath + "assets/css/"
+        , img: buildpath + "assets/img/"
+        , lib: buildpath + "assets/lib"
+      }
+  }
+  ;
+
+gulp.task('browser-sync', function() {
+    browserSync.init(null, {
+      proxy: {
+        host: "localhost"
+      }
+    });
 });
 
-gulp.task('images', function() {
-  return gulp.src('../assets/images/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('../build/assets/images'))
+// dev tasks
+gulp.task("dev-php", function () {
+  gulp.src(paths.dev.phpfiles)
+      .pipe(browserSync.reload({stream:true}));
 });
 
-gulp.task('styles', function() {
-  return gulp.src('../assets/scss/*.scss', { style: 'expanded' })
-    // del(["../build/assets/stylesheets/main.min.css"], {force : true})
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(autoprefixer('last 2 version'))
-    .pipe(concat('main.css'))
-    .pipe(minifycss())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('../build/assets/stylesheets'))
+gulp.task("dev-html", function () {
+  gulp.src(paths.dev.htmlfiles)
+     .pipe(browserSync.reload({stream:true}));
 });
 
-gulp.task('depcss', function() {
-  return gulp.src('../assets/dependencies/css/*.css', { style: 'expanded' })
-    .pipe(autoprefixer('last 2 version'))
-    .pipe(concat('dependencies.css'))
-    .pipe(minifycss())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('../build/assets/dependencies/css'))
+gulp.task("dev-styles", function() {
+  var opts = { style: "expanded" };
+  gulp.src(paths.dev.scssfiles)
+      .pipe(sass(opts))
+      .pipe(autoprefix("last 2 versions", "> 1%", "Explorer 7", "Android 2"))
+      .pipe(gulp.dest(paths.dev.css))
+      .pipe(browserSync.reload({stream:true}));
 });
 
-gulp.task('depjs', function() {
-  return gulp.src('../assets/dependencies/js/*.js', { style: 'expanded' })
-    .pipe(concat('dependencies.min.js'))
-    .pipe(gulp.dest('../build/assets/dependencies/js'))
-});
-
-
-gulp.task('scripts', function() {
-  return gulp.src('../assets/js/**/*.js')
-    //del(["../build/assets/js/main.min.js"], {force : true})
-    .pipe(concat('main.js'))
-    .pipe(jsmin())
-    .pipe(uglify())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('../build/assets/js'))
-});
-
-gulp.task('watch', function() {
-
-  // Watch .scss files
-  gulp.watch('../assets/stylesheets/**/*.scss', ['styles']);
-
-  // Watch .js files
-  gulp.watch('../assets/javascripts/**/*.js', ['scripts']);
-
-  // Watch image files
-  gulp.watch('../assets/images/**/*', ['images']);
+gulp.task("dev-jsconcat", function(){
+  gulp.src(paths.dev.jsfiles)
+      .pipe(concat("main.js"))
+      .pipe(gulp.dest(paths.dev.js))
+      ;
 
 });
+
+// build tasks
+// CSS concat and minify
+gulp.task("cssmin", function(){
+  gulp.src(paths.dev.cssfiles)
+      .pipe(concat("cssupload.css"))
+      .pipe(cssmin())
+      .pipe(rename({suffix: ".min"}))
+      .pipe(gulp.dest(paths.build.css))
+      .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task("jsmin", function(){
+  gulp.src(paths.dev.js2build)
+        .pipe(jsmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(paths.build.js))
+        .pipe(browserSync.reload({stream:true}))
+        ;
+});
+
+// Moves the libraries to build/assets/lib folder
+gulp.task("build-libs", function(){
+  gulp.src(paths.dev.libfiles)
+      .pipe(gulp.dest(paths.build.lib))
+      ;
+});
+
+// watch html, scss and js files
+gulp.task("watch", function () {
+  gulp.watch(paths.dev.scssfiles, ["dev-styles"]);
+  gulp.watch(paths.dev.cssfiles,  ["cssmin"]);
+  gulp.watch(paths.dev.jsfiles,   ["dev-jsconcat"]);
+  gulp.watch(paths.dev.jsfiles,   ["jsmin"]);
+  gulp.watch(paths.dev.htmlfiles, ["dev-html"]);
+  gulp.watch(paths.dev.phpfiles,  ["dev-php"]);
+});
+
+// tasks
+// gulp.task("build",  ["sendfonts","sendphp","imgmin","jsmin","cssmin"]);
+//gulp.task("default", ["dev-styles", "dev-jsconcat", 'browser-sync', "watch"]);
+gulp.task("default", ["dev-styles", "dev-jsconcat", "watch"]);
+
+
+
+
+
+// Toni
+
+gulp.task('bower', function(cb){
+  bower.commands.install([], {save: true}, {})
+    .on('end', function(installed){
+      cb(); // notify gulp that this task is finished
+    });
+});
+
+gulp.src(['./bower.json'])
+  .pipe(install()
+);
