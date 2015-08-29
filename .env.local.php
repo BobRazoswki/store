d1954a7ae04a2f4fc2b3bccac392026ebe0ca734
BobRazoswki <Bob@MacBook-Pro.netgear.com>
2015-08-29 16:30:00 +0200

    init project with gulp

---
+++ .env.local.php
@@ -0,0 +1,15 @@
+<?php
+
+/*----------------------------------------------------*/
+// Local environment vars
+/*----------------------------------------------------*/
+return array(
+
+    'DB_NAME'       => 'dbstore',
+    'DB_USER'       => 'root',
+    'DB_PASSWORD'   => 'root',
+    'DB_HOST'       => 'localhost:8889',
+    'WP_HOME'       => 'http://localhost:8888/store/htdocs/',
+    'WP_SITEURL'    => 'http://localhost:8888/store/htdocs/cms'
+
+);
---
+++ config/environment.php
@@ -0,0 +1,25 @@
+<?php
+
+/*----------------------------------------------------*/
+// Define your environments
+/*----------------------------------------------------*/
+return array(
+
+    'local'         => 'MacBook-Pro.netgear.com',
+    'production'    => 'your-production-hostname'
+
+);
+
+
+// return function()
+// {
+//     // Check for the environment variable
+//     if ('varValue' === getenv('varName'))
+//     {
+//         // Return the environment file slug name: .env.{$slug}.php
+//         return 'local';
+//     }
+//
+//     // Else if no environment variable found... it might be a production environment...
+//     return 'production';
+// }
